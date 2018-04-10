3ce1f5cb84fd35ecf9258b544b70fa78311262f6
cdurazzo <christopher.durazzo@wagner.edu>
2018-04-10 11:25:05 -0400

    made pen thicker, some minor differences

--- draw.py
+++ draw.py
@@ -1,9 +1,10 @@
 import turtle
 
 painter = turtle.Turtle()
+painter.pensize(20)
 
-for _ in range(18):
-    painter.forward(100)
+for _ in range(100):
+    painter.forward(150)
     painter.left(100)
 
 turtle.done()
